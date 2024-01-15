4764c209a6af2c527b9fd124948fe9d432581def
Kobi96 <tobiasolveira@outlook.com>
2024-01-15 14:49:23 -0300

    updates 15-1-24

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,28 +1,14 @@
-import React, { useState, useEffect, useRef } from "react";
-import Button from "./Button";
-import axios from "axios";
+import React, { useRef, useEffect } from "react";
 import { Highlight, themes } from "prism-react-renderer";
-import { contactData, toastMessages } from "../assets/lib/data.tsx";
+import { contactData } from "../assets/lib/data.tsx";
 import { useSectionInView } from "../assets/lib/hooks";
 import { useLanguage } from "../context/language-context";
-import { ToastContainer, toast } from "react-toastify";
-import { useTheme } from "../context/theme-context";
 import { motion, useScroll, useTransform } from "framer-motion";
 import "react-toastify/dist/ReactToastify.css";
 
 const Contact: React.FC = () => {
-  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "";
-
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [subject, setSubject] = useState<string>("");
-  const [message, setMessage] = useState<string>("");
-  const [cursor, setCursor] = useState<string>("");
-  const [lastUpdatedField, setLastUpdatedField] = useState<string | null>(null);
   const { ref } = useSectionInView("Contact");
   const { language } = useLanguage();
-  const { theme } = useTheme();
-  const [error, setError] = useState<string | null>(null);
 
   const animationReference = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -32,144 +18,13 @@ const Contact: React.FC = () => {
   const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
-  const notifySentForm: React.FormEventHandler<HTMLFormElement> = async (e) => {
-    setError(null);
-    console.log(error);
-
-    e.preventDefault();
-    const data = new FormData(e.currentTarget);
-
-    try {
-      const response = await axios.post(apiBaseUrl, data);
-      console.log(response);
-      if (language === "ESP") {
-        toast.success(toastMessages.successEmailSent.esp);
-      } else {
-        toast.success(toastMessages.successEmailSent.en);
-      }
-    } catch (error) {
-      console.log(error);
-      if (language === "ESP") {
-        toast.error(toastMessages.failedEmailSent.esp);
-      } else {
-        toast.error(toastMessages.failedEmailSent.en);
-      }
-      setError("An Error occured, try again later");
-    }
-  };
-
-  const handleInputFocus = (fieldName: string) => {
-    setCursor(`${fieldName}${cursor}`);
-  };
-
-  const wordWrap = (
-    text: string,
-    maxLineLength: number,
-    indentation: string
-  ) => {
-    const words = text.split(" ");
-    const lines: string[] = [];
-    let currentLine = "";
-
-    words.forEach((word) => {
-      if (currentLine.length + word.length <= maxLineLength) {
-        currentLine += word + " ";
-      } else {
-        lines.push(currentLine.trim());
-        currentLine = `${indentation}${word} `;
-      }
-    });
-
-    if (currentLine) {
-      lines.push(currentLine.trim());
-    }
-
-    return lines.join("\n");
-  };
-
-  const handleInputChange = (
-    e:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-
-    if (name === "name") {
-      setName(value);
-    } else if (name === "email") {
-      setEmail(value);
-    } else if (name === "subject") {
-      setSubject(value);
-    } else if (name === "message") {
-      setMessage(value);
-    }
-
-    setLastUpdatedField(name);
-  };
-
-  const [cursorBlink, setCursorBlink] = useState<boolean>(true);
-
   useEffect(() => {
-    const blinkInterval = setInterval(() => {
-      setCursorBlink((prev) => !prev);
-    }, 400);
+    const blinkInterval = setInterval(() => {}, 400);
 
     return () => {
       clearInterval(blinkInterval);
     };
   }, []);
-
-  const codeSnippet = `
-import  { useState } from "react";
-
-// 🌈 Spreading Stardust: 
-// Crafting Cosmic Email 🌌
-
-const [sender, setSender] = "${name}${
-    lastUpdatedField === "name" ? (cursorBlink ? "|" : " ") : ""
-  }🚀";
-const [recipient, setRecipient] = "${email}${
-    lastUpdatedField === "email" ? (cursorBlink ? "|" : " ") : ""
-  }📧";
-const [subject, setSubject] = \n"${subject}${
-    lastUpdatedField === "subject" ? (cursorBlink ? "|" : " ") : ""
-  }✨";
-const [message, setMessage] = 
-\`Hello, intrepid traveler! 👋\n
-Across the cosmos, a message for you:\n
-"${wordWrap(message, 40, " ")}${
-    lastUpdatedField === "message" ? (cursorBlink ? "|" : " ") : ""
-  }"\n
-Wishing you stardust dreams,\n
-${name}${lastUpdatedField === "name" ? (cursorBlink ? "|" : " ") : ""}
-\``;
-
-  //   const codeSnippet2 = `
-  // // 🚀 Initiating Quantum Email Transmission 🪐
-  // const launchEmail = async () => {
-  //   try {
-  //     const response = await fetch('https://alpaycelik.dev/send',{
-  //     method: 'POST',
-  //     headers: {'Content-Type': 'application/json'},
-  //     body: JSON.stringify({
-  //      sender,
-  //      recipient,
-  //      subject,
-  //      message
-  //     })
-  //    });
-
-  //    if (response.ok) {
-  //    console.log('🌌 Transmission successful!');
-  //    } else {
-  //    console.error('🌠 Cosmic glitch encountered...');
-  //    }
-  //   } catch (error) {
-  //   console.error('🌪 Quantum disturbance detected:', error);
-  //   }
-  // };
-  // // 🚀 Ready for Liftoff? 🛸
-  // launchEmail();`;
 
   return (
     <React.Fragment>
@@ -204,15 +59,20 @@ ${name}${lastUpdatedField === "name" ? (cursorBlink ? "|" : " ") : ""}
             </h2>
           </motion.div>
         </div>
-        <div className="flex flex-row justify-center items-start px-32 pt-32 mb-32 max-lg:flex-col max-lg:p-10">
-          <div className="w-1/2  bg-[--darkblue] text-[--white] flex flex-col justify-center items-start gap-24 rounded-2xl p-20 border-solid border-[0.4rem] border-[--lightblue] hover:border-orange duration-500 transition-all  quote-outer-container text-left max-lg:hidden cursor-progress">
+        <div className="flex flex-row justify-center items-center px-10 pt-32 mb-32 max-lg:flex-col max-lg:p-10">
+          <div className="w-full bg-[--darkblue] text-[--white] flex flex-col justify-center items-start gap-4 rounded-2xl p-8 border-solid border-[0.4rem] border-[--lightblue] hover:border-orange duration-500 transition-all  quote-outer-container text-left max-lg:hidden cursor-progress">
             <Highlight
-              code={codeSnippet}
-              language="tsx"
+              code={`
+console.log("Contact Me:");
+console.log("Email: tobiasolveira@outlook.com");
+console.log("LinkedIn: https://www.linkedin.com/in/tobias-olveira-52a0311b7/");
+console.log("GitHub: https://github.com/Kobi96");
+              `}
+              language="javascript"
               theme={themes.nightOwl}
             >
               {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={`${className} text-4xl `} style={style}>
+                <pre className={`${className} text-2xl`} style={style}>
                   {tokens.map((line, i) => (
                     <div {...getLineProps({ line, key: i })}>
                       {line.map((token, key) => (
@@ -224,120 +84,6 @@ ${name}${lastUpdatedField === "name" ? (cursorBlink ? "|" : " ") : ""}
               )}
             </Highlight>
           </div>
-          <form
-            className="flex flex-col gap-6 justify-center items-center  px-32 w-1/2 max-lg:w-full max-lg:p-10"
-            onSubmit={notifySentForm}
-            autoComplete="off"
-          >
-            {contactData.inputfields.map((input, index) => (
-              <input
-                key={index}
-                type={input.type}
-                placeholder={
-                  language === "ESP"
-                    ? `${input.placeholder.esp}`
-                    : `${input.placeholder.en}`
-                }
-                name={input.name}
-                value={
-                  input.name === "name"
-                    ? name
-                    : input.name === "email"
-                    ? email
-                    : input.name === "subject"
-                    ? subject
-                    : message
-                }
-                required
-                onFocus={() => {
-                  handleInputFocus(input.name);
-                  setLastUpdatedField(input.name);
-                }}
-                onMouseEnter={() => {
-                  handleInputFocus(input.name);
-                  setLastUpdatedField(input.name);
-                }}
-                onChange={handleInputChange}
-                className={`${
-                  theme === "dark"
-                    ? "bg-[--blackblue] dark-mode-shadow "
-                    : "bg-[--icewhite] dark-shadow "
-                }`}
-              />
-            ))}
-            <textarea
-              rows={contactData.textarea.rows}
-              placeholder={
-                language === "ESP"
-                  ? `${contactData.textarea.placeholder.esp}`
-                  : `${contactData.textarea.placeholder.en}`
-              }
-              name={contactData.textarea.name}
-              onFocus={() => {
-                handleInputFocus(contactData.textarea.name);
-                setLastUpdatedField(contactData.textarea.name);
-              }}
-              onMouseEnter={() => {
-                handleInputFocus(contactData.textarea.name);
-                setLastUpdatedField(contactData.textarea.name);
-              }}
-              onChange={handleInputChange}
-              className={`${
-                theme === "dark"
-                  ? "bg-[--blackblue] dark-mode-shadow"
-                  : "bg-[--icewhite] dark-shadow"
-              }`}
-            />
-            <div className="privacy-checkbox flex gap-16">
-              <label
-                className="block w-2 h-2 cursor-pointer"
-                htmlFor="checkbox-label"
-              >
-                <input
-                  type="checkbox"
-                  required
-                  name="checkbox-label"
-                  id="checkbox-label"
-                />
-                <span className="checkbox"></span>
-              </label>
-              <p>
-                {language === "ESP"
-                  ? `${contactData.privacyOptIn.checkbox.esp}`
-                  : `${contactData.privacyOptIn.checkbox.en}`}
-              </p>
-            </div>
-            <p>
-              {language === "ESP"
-                ? `${contactData.privacyOptIn.description.esp}`
-                : `${contactData.privacyOptIn.description.en}`}
-            </p>
-            <Button
-              value={
-                language === "ESP"
-                  ? `${contactData.button.value.esp}`
-                  : `${contactData.button.value.en}`
-              }
-              iconSVG={contactData.icon}
-              buttoncolor={contactData.colors.main}
-              iconcolor={contactData.colors.icon}
-              type="submit"
-              elementType="input"
-            />
-            <ToastContainer
-              className="w-max text-3xl block p-3 max-lg:w-full "
-              position="bottom-center"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme={theme}
-            />
-          </form>
         </div>
       </section>
     </React.Fragment>
